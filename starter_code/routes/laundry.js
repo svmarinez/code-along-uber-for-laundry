cc6a5721d7ed05ca5c3be2c1f81a9ac0c2b2488c
Verónica Reina <veroreinah@gmail.com>
2018-07-13 11:38:55 +0200

    Iteration 6

--- starter_code/routes/laundry.js
+++ starter_code/routes/laundry.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { ensureLoggedIn } = require("../middleware/ensureLogin");
 const User = require("../models/User");
+const LaundryPickup = require("../models/Laundry-pickup");
 
 router.get("/dashboard", ensureLoggedIn("/login"), (req, res, next) => {
   res.render("laundry/dashboard");
@@ -30,4 +31,45 @@ router.get("/launderers", ensureLoggedIn("/login"), (req, res, next) => {
     next(err);
   })
 });
+
+router.get("/launderers/:id", ensureLoggedIn("/login"), (req, res, next) => {
+  User.findById(req.params.id)
+  .then(launderer => {
+    res.render("laundry/launderer-profile", {theLaunderer: launderer})
+  })
+  .catch(err => {
+    next(err);
+  })
+});
+
+router.post("/laundry-pickups", ensureLoggedIn("/login"), (req, res, next) => {
+  const user = req.user._id;
+  const { pickupDate, laundererId } = req.body;
+
+  var fieldsPromise = new Promise((resolve, reject) => {
+    if (pickupDate === "" || laundererId === "") {
+      reject(new Error("Invalid pickup"));
+    } else {
+      resolve();
+    }
+  });
+
+  fieldsPromise.then(() => {
+    const thePickup = new LaundryPickup({
+      pickupDate,
+      launderer: laundererId,
+      user
+    });
+
+    return thePickup.save();
+  })
+  .then(pickup => {
+    req.flash("success", "Your pickup has been scheduled");
+    res.redirect("/dashboard");
+  })
+  .catch(err => {
+    next(err);
+  });
+});
+
 module.exports = router;
